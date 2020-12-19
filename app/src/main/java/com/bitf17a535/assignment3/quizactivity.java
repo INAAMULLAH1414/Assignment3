508bccf4960bfd6df1ca8bcbb3b8a516e4fc26f6
INAAMULLAH1414 <inamullah1414@gmail.com>
2020-12-19 16:56:26 +0500

    quizapp activity ended quizactivity started

---
+++ app/src/main/java/com/bitf17a535/assignment3/quizactivity.java
@@ -0,0 +1,14 @@
+package com.bitf17a535.assignment3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class quizactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quizactivity);
+    }
+}
--- app/src/main/java/com/bitf17a535/assignment3/quizapp.java
+++ app/src/main/java/com/bitf17a535/assignment3/quizapp.java
@@ -2,7 +2,18 @@ package com.bitf17a535.assignment3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class quizapp extends AppCompatActivity {
 
@@ -10,5 +21,20 @@ public class quizapp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quizapp);
+
+        Button startbutton=(Button)findViewById(R.id.button);
+        final EditText nametext=(EditText)findViewById(R.id.editName);
+
+        startbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name=nametext.getText().toString();
+                Intent intent=new Intent(getApplicationContext(),quizactivity.class);
+                intent.putExtra("myname",name);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
